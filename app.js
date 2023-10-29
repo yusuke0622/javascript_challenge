fe1d216cdff34783b23076fcc2366b0c19b8eccf
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-10-29 14:47:57 +0000

    Update app_js

--- app.js
+++ app.js
@@ -1,10 +1,62 @@
-let max = 100;
-let num = 1;
-let count =0;
-
-while(num < max){
-    num = num * 2;
-    count = count + 1;
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+    alert('グー・チョキ・パーのいずれかを入力してください');
+    user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 }
 
-alert('2掛け続けて' + max + 'を超えるのに必要なだった回数は' + count + '回です');
+let js_hand = getJShand();
+let judge = winLose(user_hand, js_hand);
+
+if (user_hand != null){
+    alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else{
+    alert("またチャレンジしてね。")
+}
+
+
+function getJShand(){
+    let js_hand_num = Math.floor( Math.random() * 3 );
+    let hand_name;
+    
+    if(js_hand_num == 0){
+        hand_name = "グー";
+    } else if(js_hand_num == 1){
+        hand_name = "チョキ";
+    } else if(js_hand_num == 2){
+        hand_name = "パー";
+    }
+    
+    return hand_name;
+}
+
+function winLose(user, js){
+    let winLoseStr;
+    
+    if(user == "グー"){
+        if(js == "グー"){
+            winLoseStr = "あいこ";
+        } else if(js == "チョキ"){
+            winLoseStr = "勝ち";
+        } else if(js == "パー"){
+            winLoseStr = "負け";
+        }
+    } else if(user == "チョキ"){
+        if(js == "グー"){
+            winLoseStr = "勝ち";
+        } else if(js == "チョキ"){
+            winLoseStr = "あいこ";
+        } else if(js == "パー"){
+            winLoseStr = "負け";
+        }
+    } else if(user == "パー"){
+        if(js == "グー"){
+            winLoseStr = "負け";
+        } else if(js == "チョキ"){
+            winLoseStr = "勝ち";
+        } else if(js == "パー"){
+            winLoseStr = "あいこ";
+        }
+    } 
+    
+    return winLoseStr;
+}
